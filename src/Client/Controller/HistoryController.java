c53e6957a03cff63c18b965d992c6aa841e974aa
Minh <minhhuu.ptit@gmail.com>
2025-10-28 02:36:11 +0700

    Merge remote-tracking branch 'origin/master' into feature/minhdh
    # Conflicts:
    #	src/Server/ClientHandler.java

--- src/Client/Controller/HistoryController.java
+++ src/Client/Controller/HistoryController.java
@@ -4,6 +4,9 @@ import Client.ClientConnection;
 import common.Request;
 import common.Response;
 
+import java.util.List;
+import java.util.Map;
+
 public class HistoryController {
     private final ClientConnection connection;
 
@@ -11,9 +14,18 @@ public class HistoryController {
         this.connection = connection;
     }
 
-    public Response getMatchHistory() {
-        Request req = new Request("history.get_history");
+    public Response getHistory() {
+        Request req = new Request("getHistory");
         return connection.sendRequest(req);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Map<String, Object>> getHistoryList(Response response) {
+        Object data = response.get("historyList");
+        if (data instanceof List) {
+            return (List<Map<String, Object>>) data;
+        }
+        return null;
     }
 }
 
--- src/Server/DAO/MatchDAO.java
+++ src/Server/DAO/MatchDAO.java
@@ -1,73 +1,58 @@
-﻿package Server.DAO;
+package Server.DAO;
 
 import Server.db.DatabaseConnection;
 
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MatchDAO {
-
-    public List<Map<String, Object>> getMatchHistory() {
+    
+    public List<Map<String, Object>> getAllMatchHistory() {
         List<Map<String, Object>> historyList = new ArrayList<>();
         
         String query = "SELECT " +
-                "md1.MatchId, " +
+                "m.id as matchId, " +
                 "u1.username as player1, " +
                 "u2.username as player2, " +
                 "md1.total_score as score1, " +
                 "md2.total_score as score2, " +
                 "md1.play_time as time1, " +
                 "md2.play_time as time2, " +
-                "COALESCE(md1.created_at, md2.created_at) as match_date " +
-                "FROM MatchDetails md1 " +
-                "JOIN MatchDetails md2 ON md1.MatchId = md2.MatchId AND md1.UserId != md2.UserId " +
+                "md1.created_at as matchDate " +
+                "FROM Matches m " +
+                "JOIN MatchDetails md1 ON m.id = md1.MatchId " +
                 "JOIN Users u1 ON md1.UserId = u1.id " +
+                "JOIN MatchDetails md2 ON m.id = md2.MatchId AND md1.UserId != md2.UserId " +
                 "JOIN Users u2 ON md2.UserId = u2.id " +
                 "WHERE md1.UserId < md2.UserId " +
-                "ORDER BY match_date DESC, md1.MatchId DESC " +
-                "LIMIT 100";
-
+                "ORDER BY md1.created_at DESC";
+        
         try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement ps = connection.prepareStatement(query);
              ResultSet rs = ps.executeQuery()) {
-
+            
             while (rs.next()) {
-                Map<String, Object> matchData = new HashMap<>();
-                matchData.put("match_id", rs.getString("MatchId"));
-                matchData.put("player1", rs.getString("player1"));
-                matchData.put("player2", rs.getString("player2"));
+                Map<String, Object> match = new HashMap<>();
+                match.put("matchId", rs.getString("matchId"));
+                match.put("player1", rs.getString("player1"));
+                match.put("player2", rs.getString("player2"));
+                match.put("score1", rs.getInt("score1"));
+                match.put("score2", rs.getInt("score2"));
+                match.put("time1", rs.getDouble("time1"));
+                match.put("time2", rs.getDouble("time2"));
+                match.put("matchDate", rs.getTimestamp("matchDate"));
                 
-                int score1 = rs.getInt("score1");
-                int score2 = rs.getInt("score2");
-                matchData.put("score", score1 + " - " + score2);
-                
-                matchData.put("time1", rs.getDouble("time1"));
-                matchData.put("time2", rs.getDouble("time2"));
-                
-                // Try to get date, fallback to MatchId if column doesn't exist
-                try {
-                    Timestamp timestamp = rs.getTimestamp("match_date");
-                    if (timestamp != null) {
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                        matchData.put("date", sdf.format(timestamp));
-                    } else {
-                        matchData.put("date", "N/A");
-                    }
-                } catch (SQLException e) {
-                    matchData.put("date", "N/A");
-                }
-                
-                historyList.add(matchData);
+                historyList.add(match);
             }
         } catch (SQLException e) {
             System.err.println("Get match history error: " + e.getMessage());
             e.printStackTrace();
         }
-
+        
         return historyList;
     }
 }
+
