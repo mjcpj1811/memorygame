cd63bff0836d69c056806ab60e0282131eafebe9
mjcpj1811 <truongnamkhanhkm2004@gmail.com>
2025-10-28 08:36:52 +0700

    init

---
+++ src/Server/handler/HandlerRegistry.java
@@ -0,0 +1,23 @@
+package Server.handler;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerRegistry {
+
+    private final Map<String, RequestHandler> handlers = new HashMap<>();
+
+    public HandlerRegistry() {
+        register("login", new LoginHandler());
+        register("register", new RegisterHandler());
+    }
+
+    public void register(String action, RequestHandler handler) {
+        handlers.put(action, handler);
+    }
+
+    public RequestHandler getHandler(String action) {
+        return handlers.get(action);
+    }
+}
